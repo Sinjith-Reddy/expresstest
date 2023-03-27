8e8d2dd03a49085d8567a49adc7fe94dd744174a
Sinjith-Reddy <58818193+Sinjith-Reddy@users.noreply.github.com>
2023-03-27 14:30:06 +0530

    Update index.js

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello, This is Sinjith Reddy');
+  res.send('Hello All');
 });
 
 /* istanbul ignore next */
